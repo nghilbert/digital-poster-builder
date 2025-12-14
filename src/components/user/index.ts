0b63d5bf8c9a87174262af893458bc415767da4d
Nate <nathanghilbert@gmail.com>
2025-12-14 12:19:46 -0600

    user component typing is now inferred

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -1,9 +1,7 @@
-import type { UserComponent } from "@craftjs/core";
-
 import { Container } from "./Container";
 import { RootBox } from "./RootBox";
 
 export const userComponents = {
 	Container,
 	RootBox,
-} as const satisfies Record<string, UserComponent>;
+};
